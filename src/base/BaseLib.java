e18fa6910e9fc4690e299c9794647514490534af
Yash Shukla <yashshukla183@gmail.com>
2017-09-20 15:24:08 +0530

    Update BaseLib.java

--- src/base/BaseLib.java
+++ src/base/BaseLib.java
@@ -20,8 +20,8 @@ import org.testng.annotations.Parameters;
 import java.net.URL;
 
 public class BaseLib {
-	  public static final String USERNAME = "yash122";
-	  public static final String AUTOMATE_KEY = "9xqqnDPP97twQ2XraKjW";
+	  public static final String USERNAME = "yash125";
+	  public static final String AUTOMATE_KEY = "AyL3ZyW4b7HXqPssfgDu";
 	  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 	
 	public WebDriver driver;
